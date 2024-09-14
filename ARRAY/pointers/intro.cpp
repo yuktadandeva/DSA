947b76e505de0bf861b79452a655d7fe6169df42
Yukta <yuktadandeva@gmail.com>
2024-09-14 15:48:16 +0530

    up

--- ARRAY/pointers/intro.cpp
+++ ARRAY/pointers/intro.cpp
@@ -30,11 +30,24 @@ int main() {
     // cout<<*xptr<<endl;
         
     // cout<<chptr<<endl;
-        
-     int x = 1010;
-     int * xptr =  &x;
-     cout<<xptr<<endl;
-    xptr = xptr + 2;
-    cout<<xptr;
+
+    //  int x = 1010;
+    //  int * xptr =  &x;
+    //  cout<<xptr<<endl;
+    // xptr = xptr + 2;
+    // cout<<xptr;
+    
+    // char ch = 's';
+    // void* chptr = ch;
+    
+    // int p = 20;
+    // int* ptr = &p;
+    // cout<<&ptr<<endl;
+    // int** ptrptr = &ptr;
+    // cout<<ptrptr;
+    
+    int arr[] = {10,20,30,40};
+    cout<<arr<<" "<<&arr[0]<<endl;
+    cout<<arr+1<<" "<<&arr[1];
     return 0;
 }
